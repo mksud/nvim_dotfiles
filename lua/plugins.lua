ee776944ae64fbd7f90faf811ceb303144962185
Sudhakar M K <sudhakar.m.kumar@gmail.com>
2022-06-13 09:34:14 +0530

    add stylua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,7 +8,7 @@ function M.setup()
   local conf = {
     display = {
       open_fn = function()
-        return require("packer.util").float { border = "rounded" }
+        return require('packer.util').float { border = 'rounded' }
       end,
     },
   }
@@ -17,108 +17,107 @@ function M.setup()
   -- Run PackerCompile if there are changes in this file
   local function packer_init()
     local fn = vim.fn
-    local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+    local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
     if fn.empty(fn.glob(install_path)) > 0 then
       packer_bootstrap = fn.system {
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
+        'git',
+        'clone',
+        '--depth',
+        '1',
+        'https://github.com/wbthomason/packer.nvim',
         install_path,
       }
       vim.cmd [[packadd packer.nvim]]
     end
-    vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
+    vim.cmd 'autocmd BufWritePost plugins.lua source <afile> | PackerCompile'
   end
 
   -- Plugins
   local function plugins(use)
-    use { "wbthomason/packer.nvim" }
+    use { 'wbthomason/packer.nvim' }
 
     use {
-      "tpope/vim-surround",
-      keys = {"c", "d", "y"}
-		  -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
-		  --   -- setup = function()
-		  --       --  vim.o.timeoutlen = 500
-		  --         -- end
-		  --         },
-		  --
+      'tpope/vim-surround',
+      keys = { 'c', 'd', 'y' },
+      -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+      --   -- setup = function()
+      --       --  vim.o.timeoutlen = 500
+      --         -- end
+      --         },
+      --
     }
 
     use {
-      "tpope/vim-fugitive",
+      'tpope/vim-fugitive',
       cmd = {
-        "G",
-        "Git",
-        "Gdiffsplit",
-        "Gvdiffsplit",
-        "Gread",
-        "Gwrite",
-        "Ggrep",
-        "GMove",
-        "GDelete",
-        "GBrowse",
-        "GRemove",
-        "GRename",
-        "Glgrep",
-        "Gedit"
+        'G',
+        'Git',
+        'Gdiffsplit',
+        'Gvdiffsplit',
+        'Gread',
+        'Gwrite',
+        'Ggrep',
+        'GMove',
+        'GDelete',
+        'GBrowse',
+        'GRemove',
+        'GRename',
+        'Glgrep',
+        'Gedit',
       },
-      ft = {"fugitive"}
+      ft = { 'fugitive' },
     }
-
 
     use {
-      "tpope/vim-commentary",
-      keys = "gc"
+      'tpope/vim-commentary',
+      keys = 'gc',
     }
-
 
     -- Treesitter
     use {
-      "nvim-treesitter/nvim-treesitter",
+      'nvim-treesitter/nvim-treesitter',
       opt = true,
-      event = "BufReadPre",
-      run = ":TSUpdate",
+      event = 'BufReadPre',
+      run = ':TSUpdate',
       config = function()
-        require("config.treesitter").setup()
+        require('config.treesitter').setup()
       end,
       requires = {
-        { "nvim-treesitter/nvim-treesitter-textobjects", event = "BufReadPre" },
-        { "JoosepAlviste/nvim-ts-context-commentstring", event = "BufReadPre" },
-      }
+        { 'nvim-treesitter/nvim-treesitter-textobjects', event = 'BufReadPre' },
+        { 'JoosepAlviste/nvim-ts-context-commentstring', event = 'BufReadPre' },
+      },
     }
 
--- Colorscheme
+    -- Colorscheme
     use {
-      "RRethy/nvim-base16",
+      'RRethy/nvim-base16',
       config = function()
         vim.opt.background = 'light'
-        vim.cmd "colorscheme base16-one-light"
-      end
+        vim.cmd 'colorscheme base16-one-light'
+      end,
     }
+
     use {
       'nvim-lualine/lualine.nvim',
       config = function()
         require('lualine').setup()
       end,
       options = {
-        icons_enabled = false,
-        theme = 'base16'
+        -- icons_enabled = false,
+        theme = 'base16',
       },
       -- requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
-  
+
     if packer_bootstrap then
-      print "Restart Neovim required after installation!"
-      require("packer").sync()
+      print 'Restart Neovim required after installation!'
+      require('packer').sync()
     end
   end
- 
+
   packer_init()
 
-  local packer = require "packer"
+  local packer = require 'packer'
   packer.init(conf)
   packer.startup(plugins)
 end
