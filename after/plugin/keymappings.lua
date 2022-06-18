3b936705c99375906881a6f73c9b7f384c4b25c3
Sudhakar M K <sudhakar.m.kumar@gmail.com>
2022-06-18 15:55:00 +0530

    add keymappings

---
+++ after/plugin/keymappings.lua
@@ -0,0 +1,11 @@
+local keymap = vim.keymap.set
+local default_opts = { noremap = true, silent = true }
+local expr_opts = { noremap = true, expr = true, silent = true }
+
+-- Visual line wraps
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", expr_opts)
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", expr_opts)
+
+-- Better indent
+keymap("v", "<", "<gv", default_opts)
+keymap("v", ">", ">gv", default_opts)
