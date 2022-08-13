36961ca0676efb1753aa58de2612bc8f2fa7219e
Sudhakar M K <sudhakar.m.kumar@gmail.com>
2022-08-13 13:19:25 +0530

    Change root directory detection for js projects

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -23,19 +23,39 @@ end
 function M.setup()
   -- LSP settings
   local lspconfig = require 'lspconfig'
+  local lspconfig_util  = require 'lspconfig.util'
 
   -- nvim-cmp supports additional completion capabilities
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
   -- Enable the following language servers
-  local servers = { 'html', 'pyright', 'tsserver', 'eslint', 'omnisharp' }
-  require('nvim-lsp-installer').setup { ensure_installed = servers }
-  for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
+  -- Override some stuff that is not working in the default lspconfig settings
+  local servers = {
+    ['html'] = {},
+    ['pyright'] = {},
+    ['omnisharp'] = {},
+    ['eslint'] = {
+      root_dir = function(fname)
+        return lspconfig_util.root_pattern('jsconfig.json', 'tsconfig.json')(fname) or lspconfig_util.root_pattern('package.json', '.git')(fname)
+      end,
+    },
+    ['tsserver'] = {
+      root_dir = function(fname)
+        return lspconfig_util.root_pattern('.eslintrc', '.eslintrc.js', '.eslintrc.cjs', '.eslintrc.yaml', '.eslintrc.yml', '.eslintrc.json')(fname)
+          or lspconfig_util.root_pattern('package.json', '.git')(fname)
+      end,
+    },
+  }
+
+  require('nvim-lsp-installer').setup { ensure_installed = vim.tbl_keys(servers) }
+
+  for name, opts in pairs(servers) do
+    local server_config = vim.tbl_extend('force', {
       on_attach = on_attach,
       capabilities = capabilities,
-    }
+    }, opts or {})
+    lspconfig[name].setup(server_config)
   end
 end
 
