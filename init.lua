5ff6b9976ccb151657134e8786676717f2fc457a
Sudhakar M K <sudhakar.m.kumar@gmail.com>
2023-04-15 11:35:45 +0530

    remove neodev

--- init.lua
+++ init.lua
@@ -37,9 +37,6 @@ require('lazy').setup({
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
     },
     config = function()
       require('config.lsp').setup()
