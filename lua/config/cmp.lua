b3c52a26ac0902e916e7bf0b5f87cbb338d992b1
Sudhakar M K <sudhakar.m.kumar@gmail.com>
2024-01-16 20:07:30 +0530

    Remove cmp config for search

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -30,14 +30,6 @@ function M.setup()
       { name = 'buffer' },
     },
   }
-
-  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      { name = 'buffer' },
-    },
-  })
 end
 
 return M
