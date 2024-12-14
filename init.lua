8fecdbb3ce295c1edff1406202cb6f6ff8aaa959
Sudhakar Minakanoor Kumar <sminakanoorkumar@commvault.com>
2024-12-14 19:09:12 +0530

    Remove cmp path source

--- init.lua
+++ init.lua
@@ -55,7 +55,6 @@ require('lazy').setup({
     end,
     dependencies = {
       'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp',
       'saadparwaiz1/cmp_luasnip',
       'L3MON4D3/LuaSnip',
